0b4282557632588af6d5f7daa973d84a8d9efe97
SatooHg <phm.2000.poc@gmail.com>
2024-07-08 13:59:22 -0700

    funcao de mostrar as tarefas na tela

--- main.py
+++ main.py
@@ -16,9 +16,16 @@ def add():
 
 
 def undo():
-    t =input('Qual tarefa deseja Remover')
+    t = input('Qual tarefa deseja Remover')
     if t in n:
         n.remove(t)
         print(f'A tarefa {t} foi removida com sucesso')
     else:
         print('nenhuma tarefa encotrada')
+def display():
+    if not n
+        print('a lista esta vazia')
+    else:
+        print('Lista de Tarefas')
+        for t in n
+            print(t)
