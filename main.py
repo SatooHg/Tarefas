27e27bb732c8051d1ff5a58b90585f3b93752fef
SatooHg <phm.2000.poc@gmail.com>
2024-07-08 13:04:00 -0700

    Criando a Funcao que adiciona tarefas

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def menu():
     print('2 Remover uma Tarafa')
     print('3 Exibir Tarefas')
     print('4 Sair')
+
+
+def add():
+    t = input('Insira um novo intem')
+    n.append(t)
+    print(f'a tarefa {t} foi inserida com sucesso')
