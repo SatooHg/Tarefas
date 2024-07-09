c97059ae8df3e52790db420bde84102fbfd6660b
SatooHg <phm.2000.poc@gmail.com>
2024-07-09 05:34:17 -0700

    cria funcao mostra

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 n = list()
 
+def mostra(text):
+    print(f"{text} ")
 
 def menu():
-    print('Menu:')
-    print('1 Adicionar uma Tarefa')
-    print('2 Remover uma Tarafa')
-    print('3 Exibir Tarefas')
-    print('4 Sair')
+    mostra('Menu:')
+    mostra('1 Adicionar uma Tarefa')
+    mostra('2 Remover uma Tarafa')
+    mostra('3 Exibir Tarefas')
+    mostra('4 Sair')
 
 
 def add():
     t = input('Insira um novo item')
 
     n.append(t)
-    print(f'a tarefa {t} foi inserida com sucesso')
+    mostra(f'a tarefa {t} foi inserida com sucesso')
 
 
 def undo():
     t = input('Qual tarefa deseja Remover')
     if t in n:
         n.remove(t)
-        print(f'A tarefa {t} foi removida com sucesso')
+        mostra(f'A tarefa {t} foi removida com sucesso')
     else:
-        print('nenhuma tarefa encotrada')
+        mostra('nenhuma tarefa encotrada')
 def display():
     if not n:
-        print('a lista esta vazia')
+        mostra('a lista esta vazia')
     else:
-        print('Lista de Tarefas')
+        mostra('Lista de Tarefas')
         for t in n:
-            print(t)
+            mostra(t)
 while True:
     menu()
     op = input('Escolha uma Opcao 1 2 3 ou 4 ')
@@ -41,7 +43,7 @@ while True:
     elif op == '3':
         display()
     elif op == '4':
-        print('Saindo...')
+        mostra('Saindo...')
         break
     else:
-        print('a opcao escolhida e ivalida ou inesxitente ')
+        mostra('a opcao escolhida e ivalida ou inesxitente ')
