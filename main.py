58e6661e0b673107f8f0d4863604651b3f722d77
SatooHg <phm.2000.poc@gmail.com>
2024-07-09 05:17:11 -0700

    corrige erros ortorgraficos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ n = list()
 
 
 def menu():
-    print('Mene:')
+    print('Menu:')
     print('1 Adicionar uma Tarefa')
     print('2 Remover uma Tarafa')
     print('3 Exibir Tarefas')
@@ -10,21 +10,22 @@ def menu():
 
 
 def add():
-    t = input('Insira um novo intem ')
+    t = input('Insira um novo item')
+
     n.append(t)
-    print(f'a tarefa {t} foi inserida com sucesso ')
+    print(f'a tarefa {t} foi inserida com sucesso')
 
 
 def undo():
-    t = input('Qual tarefa deseja Remover ')
+    t = input('Qual tarefa deseja Remover')
     if t in n:
         n.remove(t)
-        print(f'A tarefa {t} foi removida com sucesso ')
+        print(f'A tarefa {t} foi removida com sucesso')
     else:
-        print('nenhuma tarefa encotrada ')
+        print('nenhuma tarefa encotrada')
 def display():
     if not n:
-        print('a lista esta vazia ')
+        print('a lista esta vazia')
     else:
         print('Lista de Tarefas')
         for t in n:
